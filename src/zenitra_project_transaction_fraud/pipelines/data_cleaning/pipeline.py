26c427c82955bf73b6a8e8aa9a8e494d54d506cb
TurmaeUrsorum <projekbrillylutfan@gmail.com>
2025-11-24 22:00:35 +0700

    pipeline/data cleaning(Cleaning Data): bikin node dan pipeline untuk cleaning data # Description membuat sebuah node dan pipeline untuk cleaning data, output nya berupa dataset hasil cleaning dalam bentuk format parquet file, parquet file dipilih karna proses nya lebih cepat daripada csv
    #  Changes :
    
    1. init untuk catalog, seperti membuat inputan data parquet dan output nya
    2. membuat sebuah node untuk cleaning data
    3. membuat sebuah pipeline untuk cleaning data
    
    #  Results / Notes :
    
    1. pipeline dan node sudah di run dan hasil nya berjalan dengan baik melalui perintah kedro run

--- src/zenitra_project_transaction_fraud/pipelines/data_cleaning/pipeline.py
+++ src/zenitra_project_transaction_fraud/pipelines/data_cleaning/pipeline.py
@@ -3,8 +3,16 @@ This is a boilerplate pipeline 'data_cleaning'
 generated using Kedro 1.0.0
 """
 
-from kedro.pipeline import Node, Pipeline  # noqa
+from kedro.pipeline import Node, Pipeline, node  # noqa
+from .nodes import clean_data
 
 
 def create_pipeline(**kwargs) -> Pipeline:
-    return Pipeline([])
+    return Pipeline([
+        node(
+            func=clean_data,
+            inputs="raw_data",
+            outputs="data_cleaned",
+            name="clean_data_node",
+        )
+    ])
