1abd82ea8496f6a91ce12ef900a27948e1a2064f
TurmaeUrsorum <projekbrillylutfan@gmail.com>
2025-11-26 17:04:00 +0700

    penambahan clean data untuk fraud

--- src/zenitra_project_transaction_fraud/pipelines/data_cleaning/nodes.py
+++ src/zenitra_project_transaction_fraud/pipelines/data_cleaning/nodes.py
@@ -15,3 +15,16 @@ def clean_data(df: pd.DataFrame, params: tp.Dict) -> pd.DataFrame:
     df_main_features = df_main_features.drop_duplicates()
 
     return df_main_features
+
+
+def clean_data_fraud(df: pd.DataFrame) -> pd.DataFrame:
+    df = df.copy()
+
+    df = df.dropna()
+
+    df = df.drop_duplicates()
+
+    df["TransactionDate"] = pd.to_datetime(df["TransactionDate"])
+    df["PreviousTransactionDate"] = pd.to_datetime(df["PreviousTransactionDate"])
+
+    return df
