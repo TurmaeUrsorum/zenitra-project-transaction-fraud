13ddb7fa9fb5a964b1a439a1acbb5a23129e29d2
TurmaeUrsorum <projekbrillylutfan@gmail.com>
2025-11-27 14:42:08 +0700

    mantap kali ini pipeline data eda

--- src/zenitra_project_transaction_fraud/pipelines/data_preproses/nodes.py
+++ src/zenitra_project_transaction_fraud/pipelines/data_preproses/nodes.py
@@ -6,22 +6,23 @@ generated using Kedro 1.0.0
 import pandas as pd
 import numpy as np
 
+
 def age_bins_column(df: pd.DataFrame) -> pd.DataFrame:
     df = df.copy()
 
     bins = [0, 25, 45, 60, 100]
-    labels = ['Gen Z', 'Millennial', 'Gen X', 'Boomer']
+    labels = ["Gen Z", "Millennial", "Gen X", "Boomer"]
 
-    df['AgeGroup'] = pd.cut(df['CustomerAge'], bins=bins, labels=labels)
+    df["AgeGroup"] = pd.cut(df["CustomerAge"], bins=bins, labels=labels)
     print("-> Kolom 'AgeGroup' berhasil dibuat.")
 
     return df
 
+
 def recovery_days_columns(df: pd.DataFrame) -> pd.DataFrame:
     df = df.copy()
 
-
-    df['RecencyDays'] = (df['TransactionDate'] - df['PreviousTransactionDate']).dt.days #type: ignore
+    df["RecencyDays"] = (df["TransactionDate"] - df["PreviousTransactionDate"]).dt.days  # type: ignore
     print("-> Kolom 'RecencyDays' berhasil dibuat.")
 
     return df
