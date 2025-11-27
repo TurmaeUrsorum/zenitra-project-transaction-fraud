8d2243f7659ebe5345951fb6b4d899618dbeaac7
TurmaeUrsorum <projekbrillylutfan@gmail.com>
2025-11-27 11:10:48 +0700

    mantap data cleaning pipelien

--- src/zenitra_project_transaction_fraud/pipelines/data_cleaning/nodes.py
+++ src/zenitra_project_transaction_fraud/pipelines/data_cleaning/nodes.py
@@ -5,26 +5,146 @@ generated using Kedro 1.0.0
 
 import pandas as pd
 import typing as tp
+import logging
+
+log = logging.getLogger(__name__)
 
 
 def clean_data(df: pd.DataFrame, params: tp.Dict) -> pd.DataFrame:
-    df_main_features = df[params["columns"]]
+    df = df.copy()
 
-    df_main_features = df_main_features.dropna()
+    df_main_features = df.dropna(subset=params["subset"])
 
-    df_main_features = df_main_features.drop_duplicates()
+    df_main_features = df_main_features.drop_duplicates(
+        subset=params["subset"]
+    )
 
     return df_main_features
 
 
-def clean_data_fraud(df: pd.DataFrame) -> pd.DataFrame:
+def date_formated(df: pd.DataFrame, params: tp.Dict) -> pd.DataFrame:
+    df = df.copy()
+    df["TransactionDate"] = pd.to_datetime(
+        df["TransactionDate"], errors=params["errors"]
+    )
+    df["PreviousTransactionDate"] = pd.to_datetime(
+        df["PreviousTransactionDate"], errors=params["errors"]
+    )
+    df["TransactionHour"] = df["TransactionDate"].dt.hour #type: ignore
+
+    return df
+
+
+def age_formated(df: pd.DataFrame) -> pd.DataFrame:
+    df = df.copy()
+    median_age = df["CustomerAge"].median()
+    df["CustomerAge"] = df["CustomerAge"].fillna(median_age)
+
+    anomalous_age = df[(df["CustomerAge"] < 0) | (df["CustomerAge"] > 100)].shape[0]
+    if anomalous_age > 0:
+        log.info(
+            f"-> Mengoreksi {anomalous_age} data umur tidak wajar (negatif/>100) menjadi median."
+        )
+        df.loc[(df["CustomerAge"] < 0) | (df["CustomerAge"] > 100), "CustomerAge"] = (
+            median_age
+        )
+
+    return df
+
+
+def balance_formated(df: pd.DataFrame) -> pd.DataFrame:
+    df = df.copy()
+    critical_cols = ["AccountBalance", "TransactionAmount", "TransactionType"]
+    df = df.dropna(subset=critical_cols)
+    return df
+
+
+def account_id_handled(df: pd.DataFrame) -> pd.DataFrame:
+    df = df.copy()
+    missing_acc = df["AccountID"].isnull().sum()
+    if missing_acc > 0:
+        print(
+            f"-> Menghapus {missing_acc} baris dengan AccountID kosong (Data Orphan)."
+        )
+        df = df.dropna(subset=["AccountID"])
+
+    return df
+
+
+def channel_occupation_handled(df: pd.DataFrame) -> pd.DataFrame:
+    df = df.copy()
+    for col in ["Channel", "CustomerOccupation"]:
+        if df[col].isnull().sum() > 0:
+            mode_val = df[col].mode()[0]
+            df[col] = df[col].fillna(mode_val)
+            print(f"-> Mengisi '{col}' kosong dengan Modus: {mode_val}")
+
+    return df
+
+
+def teknis_fromated(df: pd.DataFrame) -> pd.DataFrame:
     df = df.copy()
 
-    df = df.dropna()
+    tech_cols = ["DeviceID", "IP Address", "MerchantID", "Location"]
+    df[tech_cols] = df[tech_cols].fillna("Unknown")
+    print(f"-> Mengisi data kosong pada {tech_cols} dengan 'Unknown'.")
 
-    df = df.drop_duplicates()
+    return df
 
-    df["TransactionDate"] = pd.to_datetime(df["TransactionDate"])
-    df["PreviousTransactionDate"] = pd.to_datetime(df["PreviousTransactionDate"])
+
+def duration_login_handled(df: pd.DataFrame) -> pd.DataFrame:
+    df = df.copy()
+
+    num_cols = ["TransactionDuration", "LoginAttempts"]
+    for col in num_cols:
+        if df[col].isnull().sum() > 0:
+            median_val = df[col].median()
+            df[col] = df[col].fillna(median_val)
+            print(f"-> Mengisi '{col}' kosong dengan Median: {median_val}")
+
+    return df
+
+
+def string_consistent(df: pd.DataFrame) -> pd.DataFrame:
+    df = df.copy()
+
+    str_cols = ["Location", "Channel", "CustomerOccupation"]
+    for col in str_cols:
+
+        df[col] = df[col].astype(str).str.strip().str.title()
+    print("-> Melakukan standardisasi format teks (Title Case) pada kolom kategori.")
+
+    return df
+
+
+def dated_formated_handled(df: pd.DataFrame) -> pd.DataFrame:
+    df = df.copy()
+
+    anomalies = df[df["TransactionDate"] < df["PreviousTransactionDate"]].shape[0]
+
+    if anomalies > (0.5 * df.shape[0]):
+        print(f"   [DETECTION] Ditemukan {anomalies} baris dengan tanggal terbalik.")
+        print(
+            "   [ACTION] Melakukan penukaran (Swap) nilai antara TransactionDate dan PreviousTransactionDate."
+        )
+
+        temp_date = df["TransactionDate"].copy()
+        df["TransactionDate"] = df["PreviousTransactionDate"]
+        df["PreviousTransactionDate"] = temp_date
+
+        print(
+            "   [SUCCESS] Kolom tanggal telah diperbaiki. Sekarang Logis (Current > Previous)."
+        )
+
+        return df
+    else:
+        print("   [INFO] Logika tanggal sudah benar.")
+
+        return df
+
+
+def dated_drop(df: pd.DataFrame) -> pd.DataFrame:
+    df = df.copy()
+    df = df.dropna(subset=["TransactionDate", "PreviousTransactionDate"])
 
     return df
--- src/zenitra_project_transaction_fraud/pipelines/data_cleaning/pipeline.py
+++ src/zenitra_project_transaction_fraud/pipelines/data_cleaning/pipeline.py
@@ -4,25 +4,89 @@ generated using Kedro 1.0.0
 """
 
 from kedro.pipeline import Node, Pipeline, node  # noqa
-from .nodes import clean_data, clean_data_fraud
+from .nodes import (
+    clean_data,
+    date_formated,
+    age_formated,
+    balance_formated,
+    account_id_handled,
+    channel_occupation_handled,
+    teknis_fromated,
+    duration_login_handled,
+    string_consistent,
+    dated_formated_handled,
+    dated_drop,
+)
 
 
 def create_pipeline(**kwargs) -> Pipeline:
     return Pipeline(
         [
-            # node untuk case segmentation
             node(
                 func=clean_data,
-                inputs=["data_raw", "params:data_cleaning_segmentation"],
-                outputs="data_cleaned",
+                inputs=["data_raw", "params:clean_data"],
+                outputs="cleaned_data",
                 name="clean_data_node",
             ),
-            # node untuk case fraud detection
             node(
-                func=clean_data_fraud,
-                inputs="data_raw",
-                outputs="data_cleaned_fraud",
-                name="clean_data_node_fraud",
+                func=date_formated,
+                inputs=["cleaned_data", "params:date_formated"],
+                outputs="date_formated",
+                name="date_formated_node",
             ),
+            node(
+                func=age_formated,
+                inputs="date_formated",
+                outputs="age_formated",
+                name="age_formated_node",
+            ),
+            node(
+                func=balance_formated,
+                inputs="age_formated",
+                outputs="balance_formated",
+                name="balance_formated_node",
+            ),
+            node(
+                func=account_id_handled,
+                inputs="balance_formated",
+                outputs="account_id_handled",
+                name="account_id_handled_node",
+            ),
+            node(
+                func=channel_occupation_handled,
+                inputs="account_id_handled",
+                outputs="channel_occupation_handled",
+                name="channel_occupation_handled_node",
+            ),
+            node(
+                func=teknis_fromated,
+                inputs="channel_occupation_handled",
+                outputs="teknis_fromated",
+                name="teknis_fromated_node",
+            ),
+            node(
+                func=duration_login_handled,
+                inputs="teknis_fromated",
+                outputs="duration_login_handled",
+                name="duration_login_handled_node",
+            ),
+            node(
+                func=string_consistent,
+                inputs="duration_login_handled",
+                outputs="string_consistent",
+                name="string_consistent_node",
+            ),
+            node(
+                func=dated_formated_handled,
+                inputs="string_consistent",
+                outputs="dated_formated_handled",
+                name="dated_formated_handled_node",
+            ),
+            node(
+                func=dated_drop,
+                inputs="dated_formated_handled",
+                outputs="data_cleaned",
+                name="dated_drop_node",
+            )
         ]
     )
