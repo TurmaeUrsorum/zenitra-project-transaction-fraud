e6ebef14ba61ea4a76ba0f30da603107e818be97
TurmaeUrsorum <projekbrillylutfan@gmail.com>
2025-11-27 17:49:43 +0700

    mantap jiwa nih pipeline modeling

--- src/zenitra_project_transaction_fraud/pipelines/data_modeling/pipeline.py
+++ src/zenitra_project_transaction_fraud/pipelines/data_modeling/pipeline.py
@@ -4,17 +4,48 @@ generated using Kedro 1.0.0
 """
 
 from kedro.pipeline import Node, Pipeline, node  # noqa
-from .nodes import modeling_kmedoids
+from .nodes import (
+    feature_transform,
+    model_IF,
+    model_KM,
+    generate_reason,
+    anomalies_detect,
+    anomali_plot,
+)
 
 
 def create_pipeline(**kwargs) -> Pipeline:
     return Pipeline(
         [
             node(
-                func=modeling_kmedoids,
-                inputs=["final_preprocessed_df", "params:modeling_cluster_params"],
-                outputs=["modeling_kmedoids", "kmedoids_model"],
-                name="modeling_kmedoids",
-            )
+                func=feature_transform,
+                inputs="feature_engineered",
+                outputs="feature_transformed",
+                name="feature_transform_node",
+            ),
+            node(
+                func=model_IF,
+                inputs="feature_transformed",
+                outputs=["anomalies_df", "model_IF"],
+                name="model_IF_node",
+            ),
+            node(
+                func=model_KM,
+                inputs="anomalies_df",
+                outputs=["clusters_df", "model_KM"],
+                name="model_KM_node",
+            ),
+            node(
+                func=anomalies_detect,
+                inputs="clusters_df",
+                outputs="anomalies_detect_df",
+                name="anomalies_detect_node",
+            ),
+            node(
+                func=anomali_plot,
+                inputs="anomalies_detect_df",
+                outputs="anomalies_plot_fig",
+                name="anomalies_plot_node",
+            ),
         ]
     )
